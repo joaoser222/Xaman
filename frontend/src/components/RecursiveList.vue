b7ef53cade2e17b6376f5178f225fac97d6b1b28
joaoser222 <joaoser222@gmail.com>
2021-03-03 08:53:26 -0300

    Adc. lista recursiva

---
+++ frontend/src/components/RecursiveList.vue
@@ -0,0 +1,33 @@
+<template>
+    <q-expansion-item
+      :header-inset-level="inset"
+      :expand-icon="nodes!=null ?'icon-chevron-down-outline':' '"
+      :expanded-icon="nodes!=null ?'icon-chevron-up-outline':' '"
+      dense
+    >
+      <template v-slot:header>
+        <q-item-section avatar >
+          <q-avatar :icon="icon"/>
+        </q-item-section>
+        <q-item-section>
+          {{label}}
+        </q-item-section>
+      </template>
+      <recursive-list
+        v-for="(node,index) in nodes" 
+        :nodes="node.children"
+        :icon="node.icon"
+        :label="node.label"
+        :inset="inset+0.2"
+        :key="index"
+      >
+    </recursive-list>
+  </q-expansion-item>
+</template>
+<script>
+  export default { 
+    props: ['icon','label','nodes','inset'],
+    name: 'recursive-list',
+    data: ()=>({}),
+  }
+</script>
