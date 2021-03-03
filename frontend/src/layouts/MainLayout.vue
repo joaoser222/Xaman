4b591d69574a9959fcd090e950ccf4f8ba40ceb9
joaoser222 <joaoser222@gmail.com>
2021-03-03 08:53:56 -0300

    Corr. modelo do layout e alterado ícones

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,22 +1,50 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header>
-      <q-btn-group spread>
-        <q-btn
-          v-for="route in routes" 
-          :key="route.path" 
-          type="a"
-          :icon="route.icon"
-          :href="base_url+'/#/'+root_path+'/'+route.path" 
-          :label="route.name"
-          class="q-pa-sm"
-          push
-          flat
-        />
-      </q-btn-group>
-    </q-header>
+      <q-drawer
+        show-if-above
+        :mini="icon_mode"
+
+        :width="200"
+        :breakpoint="500"
+        bordered
+        dark
+      >
+        <q-list>
+          <q-item>
+            <q-item-section />
+            <q-item-section avatar>
+              <q-btn
+                color="primary"
+                flat
+                round
+                @click="icon_mode = !icon_mode"
+                clickable
+                :icon="icon_mode? 'icon-chevron-forward-outline' : 'icon-chevron-back-outline'"
+                v-ripple
+              >
+              </q-btn>
+            </q-item-section>
+          </q-item>
+          <q-item
+          class="q-ma-sm rounded-borders"
+          v-for="(item,index) in $router.options.routes.find(route=>route.name=='Dashboard').children"
+          clickable
+          v-ripple
+          :key="index"
+          :to="`/dashboard/${item.path}`"
+          >
+            <q-item-section avatar>
+              <q-icon color="primary" :name="item.icon"/>
+            </q-item-section>
+            <q-item-section>{{item.name}}</q-item-section>
+          </q-item>
+        </q-list>
+      </q-drawer>
+
     <q-page-container>
-      <router-view />
+      <transition name="slither">
+        <router-view />
+      </transition>
     </q-page-container>
   </q-layout>
 </template>
@@ -26,15 +54,22 @@ export default {
   name: 'MainLayout',
   data () {
     return {
-      root_path: this.$route.path.split('/')[1],
-      base_url: window.location.origin,
       leftDrawerOpen: false,
-      routes: {}
+      icon_mode: true,
     }
-  },
-  mounted(){
-    let _this = this;
-    _this.routes = _this.$getRoutes(_this);
   }
 }
 </script>
+<style>
+.slither-enter-active, .slither-leave-active {
+  transition: transform 1s;
+}
+
+.slither-enter, .slither-leave-to {
+  transform: translateX(-100%);
+}
+
+.slither-enter-to, .slither-leave {
+  transform: translateX(0);
+}
+</style>
